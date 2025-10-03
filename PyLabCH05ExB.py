17ca60961bddff43af363be830e779f5668790e8
Jacob Yeung <xohkgolden-git@yahoo.com>
2025-10-02 22:41:52 -0700

    Improved PyLabCH05ExB.py with refined input validation and detailed output display

--- PyLabCH05ExB.py
+++ PyLabCH05ExB.py
@@ -16,36 +16,33 @@
 # Display/Print the results, using 2 decimal points for money. (3 points)
 
 from decimal import Decimal
-from fractions import Fraction #https://www.geeksforgeeks.org/python/fraction-module-python/
-from enum import Enum
+
+LABORPERHOUR = Decimal(35)
+WALLSPACEPERGALLON = Decimal(112)
+LABORPERGALLON = Decimal(8)
 
 separator = "-" * 1
-class InputKeywords(Enum):
-    WALLSPACEINSQUAREFEET = 'WALLSPACEINSQUAREFEET'
-    PRICEPERGALLON = 'PRICEPERGALLON'
-    LABORPERHOUR = 35
-
 
 # Must use at least 2 functions. One function to get input values, one to display the results and main to invoke these two. (7 points)
 def getInputedSpecifications():
+    global separator
     # Write a program that asks the user to enter the square feet of wall space to be painted and the price of the paint per gallon. (2 points)
     while True:
         try:
             wallSpaceInSquareFeet = Decimal(
-                input(separator := "Please enter the wall space in square feet (wall space in square feet): "))
-            separator = '-' * len(separator)
+                input("Please enter the wall space in square feet (sq ft): "))
             if wallSpaceInSquareFeet <= 0:
                 raise ValueError(f"Invalid input {wallSpaceInSquareFeet}. Please enter a positive numerical value larger than zero.")
-            pricePerGallon = Decimal(input("Please enter the price per Gallon of the wall (price per gallon): $"))
+            pricePerGallon = Decimal(input(separator := "Please enter the price per Gallon of the wall (price per gallon): $"))
             if pricePerGallon < 0:
                 raise ValueError(f"Invalid input {pricePerGallon}. Please enter a non-negative numerical value.")
-
+            separator = '-' * len(separator + str(pricePerGallon))
             break
         except Exception as e:
             separator = '-' * len(f"An unexpected error occurred: {e}")
             print(f"An unexpected error occurred: {e}")
 
-    return {InputKeywords.WALLSPACEINSQUAREFEET: wallSpaceInSquareFeet,InputKeywords.PRICEPERGALLON: pricePerGallon}
+    return wallSpaceInSquareFeet, pricePerGallon
 
 # Display/Print the results, using 2 decimal points for money. (3 points)
 # The program should display the following data: (3 points)
@@ -54,15 +51,23 @@ def getInputedSpecifications():
 # The cost of the paint
 # The labor charges
 # The total cost of the paint job
-def displayQuotation():
-    print()
+def displayQuotation(wallSpaceInSquareFeet: Decimal, pricePerGallon: Decimal):
+    paintCost, laborCost = Decimal(0),  Decimal(0)
 
-spec = getInputedSpecifications()
-print(separator)
+    print(f"{"Quotation":^{len(separator)}}")
+    print(separator)
+    print(f"The wall space in square feet: {wallSpaceInSquareFeet:.02f}sq ft")
+    print(f"One gallon of paint: ${pricePerGallon:.02f}/gal")
+    print(f"The number of gallons of paint required: {wallSpaceInSquareFeet / WALLSPACEPERGALLON :.02f}gal")
+    print(f"The hours of labor required: {LABORPERGALLON * wallSpaceInSquareFeet / WALLSPACEPERGALLON :.02f}hr{'' if LABORPERGALLON * wallSpaceInSquareFeet / WALLSPACEPERGALLON == 1 else 's'}")
+    print(f"The cost of the paint: ${(paintCost := wallSpaceInSquareFeet / WALLSPACEPERGALLON * pricePerGallon):.02f}")
+    print(f"The labor charges: ${(laborCost := LABORPERHOUR * LABORPERGALLON * wallSpaceInSquareFeet / WALLSPACEPERGALLON):.02f}")
+    print(f"The total cost of the paint job: ${paintCost+laborCost:.02f}")
+    print(separator)
+    print(f"{"End of Quotation":^{len(separator)}}")
 
+wallSpaceInSquareFeet, pricePerGallon = getInputedSpecifications()
+print(separator, end="\n\n")
+displayQuotation(wallSpaceInSquareFeet, pricePerGallon)
 
-
-
-# Execute the function to run the program
-# if __name__ == "__main__":
-#     calculate_calories()
+#Case 1:
